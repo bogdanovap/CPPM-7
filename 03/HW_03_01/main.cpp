11cf3bc1e237d2ee6ff1d99b3d905ecd90ac0353
Aleks Bogdanov <49127509+bogdanovap@users.noreply.github.com>
2023-01-29 17:43:14 +0300

    homework 3

---
+++ 03/HW_03_01/main.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+
+using namespace std;
+
+class Calculator{
+private:
+    double num1;
+    double num2;
+
+public:
+    Calculator() {num1 = num2 = 0;}
+    // метод должен возвращать результат сложения num1 и num2
+    double add() {
+        return num1 + num2;
+    }
+    // метод должен возвращать результат вычитания num2 из num1
+    double subtract_1_2() {
+        return num1 - num2;
+    }
+    //метод должен возвращать результат вычитания num1 из num2
+    double subtract_2_1() {
+        return num2 - num1;
+    }
+
+    // метод должен возвращать результат перемножения num1 и num2
+    double multiply(){
+        return num1 * num2;
+    }
+
+    // метод должен возвращать результат деления num1 на num2
+    double divide_1_2() {
+        if (num2!=0) return num1/num2;
+        cout << "Division by zero!" << endl;
+        return 0;
+    }
+    //метод должен возвращать результат деления num2 на num1
+    double divide_2_1() {
+        if (num1!=0) return num2/num1;
+        cout << "Division by zero!" << endl;
+        return 0;
+    }
+
+    // метод должен установить значение поля num1 равное значению аргумента num1
+    // в случае, если значение аргумента не равно 0.
+    // Возвращает true, если установка произошла успешно, в обратном случае false
+    bool set_num1(double num1=0){
+        if (num1 == 0) {
+            cout << "Не верный ввод!" << endl;
+            return false;
+        }
+        this->num1 = num1;
+        return true;
+    }
+    // метод должен установить значение поля num2 равное значению аргумента num2
+    // в случае, если значение аргумента не равно 0.
+    // Возвращает true, если установка произошла успешно, в обратном случае false
+    bool set_num2(double num2=0){
+        if (num2 == 0)  {
+            cout << "Не верный ввод!" << endl;
+            return false;
+        }
+        this->num2 = num2;
+        return true;
+    }
+
+};
+
+int main() {
+    Calculator calc = Calculator();
+
+    double num = 0;
+    while (true) {
+        cout << "Введите num1: ";
+        cin >> num;
+        if (calc.set_num1(num)) break;
+    }
+    while (true) {
+        cout << "Введите num2: ";
+        cin >> num;
+        if (calc.set_num2(num)) break;
+    }
+
+//    num1 + num2 = 9
+    cout << "num1 + num2 = " << calc.add() << endl;
+//    num1 - num2 = -3
+    cout << "num1 - num2 = " << calc.subtract_1_2() << endl;
+//    num2 - num1 = 3
+    cout << "num2 - num1 = " << calc.subtract_2_1() << endl;
+//    num1 * num2 = 18
+    cout << "num1 * num2 = " << calc.multiply() << endl;
+//    num1 / num2 = 0.5
+    cout << "num1 / num2 = " << calc.divide_1_2() << endl;
+//    num2 / num1 = 2
+    cout << "num2 / num1 = " << calc.divide_2_1() << endl;
+
+    return 0;
+}
---
+++ 03/HW_03_02/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+using namespace std;
+
+
+class Counter{
+private:
+    int counter_value;
+
+public:
+    Counter() {this->counter_value = 1;};
+    Counter(int initial) {this->counter_value = initial;};
+
+    bool execute_command(char command);
+
+    void increase(){
+        counter_value++;
+    }
+    void decrease(){
+        counter_value--;
+    }
+    int current(){
+        return counter_value;
+    }
+
+};
+
+bool Counter::execute_command(char command) {
+    switch(command){
+        case '+':
+            increase();
+            break;
+        case '-':
+            decrease();
+            break;
+        case '=':
+            cout << current() << endl;
+            break;
+        case 'x':
+            return false;
+        default:
+            cout << "unknown command" << endl;
+            return true;
+    }
+    return true;
+}
+
+int main() {
+    std::string set_start_number;
+
+    cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+    cin >> set_start_number;
+
+    int start_number = 1;
+    if (set_start_number=="да"){
+        cout << "Введите начальное значение счётчика: ";
+        cin >> start_number;
+    }
+
+    auto counter = Counter(start_number);
+
+    char command;
+    while (true) {
+        cout << "Введите команду ('+', '-', '=' или 'x'): ";
+        cin >> command;
+        if (not counter.execute_command(command)) break;
+    }
+
+    cout << "До свидания!" << endl;
+
+    return 0;
+}
